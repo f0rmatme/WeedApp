b9f14f8ab78a942a926c7614918a4261b685a628
Gershon <reydmang@myumanitoba.ca>
2018-12-01 16:54:51 -0600

    Fix weed table rest calls

--- app/api/weed.js
+++ app/api/weed.js
@@ -1,9 +1,10 @@
 module.exports = (app, db) => {
   app.get( "/weed", (req, res) =>
-    db.post.findAll().then( (result) => res.json(result) )
+    db.weed.findAll().then( (result) => res.json(result) )
   );
 
   app.get( "/weed/:id", (req, res) =>
-    db.post.findById(req.params.id).then( (result) => res.json(result))
+    db.weed.findById(req.params.id).then( (result) => res.json(result))
   );
+    
 }
