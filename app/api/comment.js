6e49a717ccf9e93dc19a605379e81458c885beef
Gershon <reydmang@myumanitoba.ca>
2018-12-01 18:54:07 -0600

    Comment creation

--- app/api/comment.js
+++ app/api/comment.js
@@ -7,4 +7,13 @@ module.exports = (app, db) => {
 		include: [db.post, db.user]
 	}).then( (result) => res.json(result) )
   );
+  app.post("/comment/:postId", (req, res) => 
+    db.comment.create({
+      postId: req.body.postId,
+      userId: req.body.userId,
+	  comment: req.body.comment,
+	  createdAt: req.body.createdAt,
+	  updatedAt: req.body.updatedAt,
+    }).then( (result) => res.json(result) )
+  );
 }
