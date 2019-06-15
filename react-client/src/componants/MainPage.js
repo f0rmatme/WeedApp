1f09b33d35fd62e5ec584c4a3b05f2fee16c26e8
Eric Kulchycki <erickulchycki@gmail.com>
2019-06-15 02:35:25 -0500

    Framework for displaying post data

--- react-client/src/componants/MainPage.js
+++ react-client/src/componants/MainPage.js
@@ -1,39 +1,44 @@
 import React from 'react';
 import axios from 'axios';
 
-const list_style = {
-  'width': '400px',
-  'height': '100%',
-  'border': '1px solid #fff'
-}
-
 class MainPage extends React.Component {
 
   state = {
-
+    posts: [],
+    loading: true,
   };
 
   componentDidMount() {
     axios.get('/posts').then(res => {
-      console.log(res.data);
+      this.setState({
+        posts: res.data,
+        loading: false,
+      })
     });
   }
 
   render() {
-
     return (
-      <div style={{'textAlign': 'center'}}>
-        <div style={{
-          'width': '40%',
-          'height': '100vh',
-          'marginRight': '25%',
-          'marginTop': '25px',
-          'marginBottom': '25px',
-          'display': 'inline-block',
-          'border': '1px solid #000'
-        }}>
-          <p>HELLO</p>
-        </div>
+      <div>
+        {!this.state.loading &&
+          <div style={{'textAlign': 'center'}}>
+            <div style={{
+              'width': '40%',
+              'height': '100vh',
+              'marginRight': '25%',
+              'marginTop': '25px',
+              'marginBottom': '25px',
+              'display': 'inline-block',
+              'border': '0.2px solid #000',
+              'borderRadius': '3px',
+              'boxShadow': '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+            }}>
+              {this.state.posts.map((post) => (
+                <p>{post.content}</p>
+              ))}
+            </div>
+          </div>
+        }
       </div>
 
     );
