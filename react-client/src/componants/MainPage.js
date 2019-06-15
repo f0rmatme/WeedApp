250aa2cb24eac6b8b0c1595a9656ce360596c1c1
Eric Kulchycki <erickulchycki@gmail.com>
2019-06-15 01:42:08 -0500

    Started MainPage

--- react-client/src/componants/MainPage.js
+++ react-client/src/componants/MainPage.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const list_style = {
+  'width': '400px',
+  'height': '100%',
+  'border': '1px solid #fff'
+}
+
 class MainPage extends React.Component {
 
   state = {
@@ -9,19 +15,22 @@ class MainPage extends React.Component {
   render() {
 
     return (
-      <div>
-
-        HELLO
+      <div style={{'textAlign': 'center'}}>
+        <div style={{
+          'width': '40%',
+          'height': '100vh',
+          'margin-right': '25%',
+          'margin-top': '25px',
+          'margin-bottom': '25px',
+          'display': 'inline-block',
+          'border': '1px solid #000'
+        }}>
+          <p>HELLO</p>
+        </div>
       </div>
 
     );
-
-
   }
-
-
-
-
 }
 
 export default MainPage;
