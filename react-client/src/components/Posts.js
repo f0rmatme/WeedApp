227973c393d23572d188d0421ff0af2e1ac1ef29
Eric Kulchycki <erickulchycki@gmail.com>
2019-08-23 20:16:43 -0500

    Added tags

--- react-client/src/components/Posts.js
+++ react-client/src/components/Posts.js
@@ -1,43 +1,59 @@
-import React from 'react';
-import axios from 'axios';
-import PostForm from './PostForm';
-import { Button, Card } from 'antd';
-import Box from './ui/Box';
+import React from "react";
+import axios from "axios";
+import PostForm from "./PostForm";
+import { Button, Card, Tag } from "antd";
+import Box from "./ui/Box";
+import Flex from "./ui/Flex";
+
+const colours = [
+  "magenta",
+  "red",
+  "volcano",
+  "orange",
+  "gold",
+  "lime",
+  "green",
+  "cyan",
+  "blue",
+  "geekblue",
+  "purple"
+];
 
 class Posts extends React.Component {
-
   state = {
     loading: true,
     visible: false,
-    posts: [],
+    posts: []
   };
 
   componentDidMount() {
-    axios.get('http://localhost:3000/posts')
-    .then((res) => {
-      this.setState({posts: res.data, loading: false});
-    }).catch((err) => {
-      console.log("YOU GOT AN ERROR NIGGA");
-    })
+    axios
+      .get("http://localhost:3000/posts")
+      .then(res => {
+        this.setState({ posts: res.data, loading: false });
+      })
+      .catch(err => {
+        console.log("YOU GOT AN ERROR NIGGA");
+      });
   }
 
   showModalForm = () => {
-    this.setState({visible: true});
+    this.setState({ visible: true });
   };
 
   handleCancel = () => {
-    this.setState({visible: false});
+    this.setState({ visible: false });
   };
 
   handleCreate = () => {
     const { form } = this.formRef.props;
     form.validateFields((err, values) => {
-      if(err){
+      if (err) {
         return;
       }
       console.log("Values: ", values);
       form.resetFields();
-      this.setState({visible: false})
+      this.setState({ visible: false });
     });
   };
 
@@ -45,21 +61,36 @@ class Posts extends React.Component {
     this.formRef = formRef;
   };
 
+  getRandomColour = () => {
+    return colours[Math.floor(Math.random() * Math.floor(11))];
+  };
+
+  processTags = tags => {
+    return tags.split(",");
+  };
+
   render() {
-    const {posts} = this.state;
+    const { posts } = this.state;
     return (
-      <Box style={{
-        'backgroundColor': 'white',
-        'minHeight': '75vh',
-        'position': 'relative',
-        'padding': '1%',
-      }}>
-        <Box style={{
-          'position': 'fixed',
-          'top': '100px',
-          'right': '50px'
-        }}>
-          <Button type="primary" onClick={this.showModalForm}>Post!</Button>
+      <Box
+        style={{
+          backgroundColor: "white",
+          minHeight: "75vh",
+          position: "relative",
+          padding: "1%",
+          paddingRight: "110px"
+        }}
+      >
+        <Box
+          style={{
+            position: "fixed",
+            top: "100px",
+            right: "50px"
+          }}
+        >
+          <Button type="primary" onClick={this.showModalForm}>
+            Post!
+          </Button>
           <PostForm
             wrappedComponentRef={this.saveFormRef}
             visible={this.state.visible}
@@ -67,45 +98,76 @@ class Posts extends React.Component {
             onCreate={this.handleCreate}
           />
         </Box>
-        <Box style={{
-          'fontWeight': 'bold',
-          'fontSize':'20px',
-          'marginLeft': '10px',
-        }}>
+        <Box
+          style={{
+            fontWeight: "bold",
+            fontSize: "20px",
+            marginLeft: "10px"
+          }}
+        >
           Posts
         </Box>
-        <Box style={{
-          'display': 'flex',
-          'flexWrap': 'wrap',
-          'justifyContent': 'flex-start'
-        }}>
-          {
-            !this.state.loading ? posts.map((post, key) => {
+        <Box
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "flex-start"
+          }}
+        >
+          {!this.state.loading ? (
+            posts.map((post, key) => {
               return (
-                <Box key={key} style={{'margin':'10px'}}>
+                <Box key={key} style={{ margin: "10px" }}>
                   <Card
-                    title={post.user.username}
+                    title={
+                      <Flex>
+                        <img
+                          style={{
+                            borderRadius: "50%",
+                            height: "25px",
+                            width: "25px",
+                            margin: "5px"
+                          }}
+                        />
+                        <Box
+                          style={{
+                            margin: "5px"
+                          }}
+                        >
+                          {post.user.username}
+                        </Box>
+                      </Flex>
+                    }
                     bordered={true}
                     hoverable={true}
                     cover={<img alt="weed" src={post.weed.pictureUrl} />}
                     style={{
-                      'width':'300px'
+                      width: "270px"
                     }}
                   >
-                    {post.content}
+                    <Box
+                      style={{
+                        paddingBottom: "10px"
+                      }}
+                    >
+                      {post.content}
+                    </Box>
+                    <Flex>
+                      {this.processTags(post.tags).map(tag => {
+                        return <Tag color={this.getRandomColour()}>{tag}</Tag>;
+                      })}
+                    </Flex>
                   </Card>
                 </Box>
               );
             })
-            :
-            (
-              <Box>LOADING WHORE </Box>
-            )
-          }
+          ) : (
+            <Box>LOADING WHORE </Box>
+          )}
         </Box>
       </Box>
     );
   }
 }
 
-export default Posts
+export default Posts;
