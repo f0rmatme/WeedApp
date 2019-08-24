5d9ed79a7af58bb891c9f01935cfa2ce40f5724a
Eric Kulchycki <erickulchycki@gmail.com>
2019-08-23 21:23:50 -0500

    Started Login

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,58 +1,62 @@
-import React from 'react';
-import './App.css';
-import {Route, Switch} from "react-router";
-import {withRouter} from 'react-router-dom';
-import MainPage from './componants/MainPage';
-import Nav from './componants/Nav';
-import Posts from './componants/Posts';
-import WeedFooter from './componants/WeedFooter';
-import TheBoys from './componants/TheBoys';
-import axios from 'axios';
-import NewCharacter from './componants/NewCharacter';
+import React, { useContext } from "react";
+import "./App.css";
+import { Route, Switch } from "react-router";
+import { withRouter } from "react-router-dom";
+import MainPage from "./componants/MainPage";
+import Nav from "./componants/Nav";
+import Posts from "./componants/Posts";
+import WeedFooter from "./componants/WeedFooter";
+import TheBoys from "./componants/TheBoys";
+import axios from "axios";
+import NewCharacter from "./componants/NewCharacter";
+import { UserContext } from "./context/userContext";
+import Login from "./componants/login";
 //import CreatePost from './componants/CreatePost';
 
-class App extends React.Component {
+const App = props => {
+  const handleHome = () => {
+    props.history.push("/");
+  };
 
-  handleHome = () => {
-    this.props.history.push('/');
-  }
+  const handlePosts = () => {
+    props.history.push("/posts/");
+  };
 
-  componentDidMount(){
-    axios.get('http://localhost:3000/posts').then((res) => {
-      console.log(res);
-    })
-  }
+  const handleTheBoys = () => {
+    props.history.push("/theboys/");
+  };
 
-  handlePosts = () => {
-    this.props.history.push('/posts/');
-  }
+  const handleNewCharacter = () => {
+    props.history.push("/newcharacter");
+  };
 
-  handleTheBoys = () => {
-    this.props.history.push('/theboys/');
-  }
+  const userCtx = useContext(UserContext);
 
-  handleNewCharacter = () => {
-    this.props.history.push('/newcharacter');
-  }
+  return (
+    <React.Fragment>
+      <Nav
+        home={handleHome}
+        posts={handlePosts}
+        theboys={handleTheBoys}
+        newchar={handleNewCharacter}
+      />
 
-  render() {
-    return (
-      <React.Fragment>
+      {userCtx.username === "" || userCtx.email === "" ? (
+        <Switch>
+          <Route exact path="/" component={Login} />
+        </Switch>
+      ) : (
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route path="/posts/" component={Posts} />
+          <Route path="/theboys/" component={TheBoys} />
+          <Route path="/newcharacter/" component={NewCharacter} />
+        </Switch>
+      )}
 
-      <Nav home={this.handleHome} posts={this.handlePosts} theboys={this.handleTheBoys} newchar={this.handleNewCharacter}/>
-
-      <Switch>
-
-        <Route exact path="/" component={MainPage}/>
-        <Route path="/posts/" component={Posts}/>
-        <Route path="/theboys/" component={TheBoys}/>
-        <Route path="/newcharacter/" component={NewCharacter}/>
-
-      </Switch>
-
-      <WeedFooter/>
-    </React.Fragment>);
-  }
-}
+      <WeedFooter />
+    </React.Fragment>
+  );
+};
 
 export default withRouter(App);
