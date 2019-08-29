f16d314f192780234ec0b803e16a12800df7cf65
Eric Kulchycki <erickulchycki@gmail.com>
2019-08-29 15:45:00 -0500

    Backend Auth
    Account creation and login

--- react-client/src/componants/login.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Login = () => {
-  return <div> This is a login Page </div>;
-};
-
-export default Login;
---
+++ routes/auth/auth.js
@@ -0,0 +1,59 @@
+var bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+module.exports = (app, db) => {
+  app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    console.log("User submitted: ", username, password);
+
+    db.user
+      .findOne({
+        where: { username: username }
+      })
+      .then(user => {
+        console.log("User Found: ", user);
+        if (user === null) {
+          res.json(false);
+        }
+        bcrypt.compare(password, user.password, function(err, result) {
+          if (result === true) {
+            console.log("Valid!");
+            let token = jwt.sign(
+              { username: user.username },
+              "XxSmonkWeedErrday420xX",
+              { expiresIn: 129600 }
+            ); // Signing the token
+            res.json({
+              sucess: true,
+              err: null,
+              token
+            });
+          } else {
+            console.log("Entered Password and Hash do not match!");
+            res.status(401).json({
+              sucess: false,
+              token: null,
+              err: "Entered Password and Hash do not match!"
+            });
+          }
+        });
+      });
+  });
+
+  app.post("/signup", (req, res) => {
+    const { username, password, email } = req.body;
+    const saltRounds = 10;
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+      db.user
+        .create({
+          username: username,
+          password: hash,
+          email: email
+        })
+        .then(result => {
+          console.log("User created: ", result);
+          res.json("user created!");
+        });
+    });
+  });
+};
