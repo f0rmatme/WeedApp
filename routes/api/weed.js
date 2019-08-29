fdc8fc6cf9f682d05643b6d91de40dd6ade745f5
Eric Kulchycki <erickulchycki@gmail.com>
2019-08-29 15:48:24 -0500

    Need access token to access routes

--- routes/api/weed.js
+++ routes/api/weed.js
@@ -1,15 +1,14 @@
-module.exports = (app, db) => {
-  app.get("/weed", (req, res) =>
-    db.weed.findAll().then((result) => res.json(result))
+module.exports = (app, db, jwtMW) => {
+  app.get("/weed", jwtMW, (req, res) =>
+    db.weed.findAll().then(result => res.json(result))
   );
 
-  app.get("/weed/:id", (req, res) =>
-    db.weed.findById(req.params.id).then((result) => res.json(result))
+  app.get("/weed/:id", jwtMW, (req, res) =>
+    db.weed.findById(req.params.id).then(result => res.json(result))
   );
 
-  app.post("/weed", (req, res) => {
+  app.post("/weed", jwtMW, (req, res) => {
     console.log(req.body);
-    db.weed.create(req.body).then((result) => res.json(result));
+    db.weed.create(req.body).then(result => res.json(result));
   });
-
-}
+};
