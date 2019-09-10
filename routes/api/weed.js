5465ef031fee35df53661e912d182249eba876f1
Eric Kulchycki <erickulchycki@gmail.com>
2019-09-10 18:09:51 -0500

    Pagination

--- routes/api/weed.js
+++ routes/api/weed.js
@@ -1,7 +1,19 @@
 module.exports = (app, db, jwtMW) => {
-  app.get("/weed", jwtMW, (req, res) =>
-    db.weed.findAll().then(result => res.json(result))
-  );
+  app.get("/weed", jwtMW, (req, res) => {
+    if (req.body.pagination) {
+      let perPage = req.body.pagination.perPage;
+      let page = req.body.pagination.page;
+
+      db.weed
+        .findAll({
+          limit: perPage,
+          offset: perPage * page
+        })
+        .then(result => res.json(result));
+    } else {
+      db.weed.findAll().then(result => res.json(result));
+    }
+  });
 
   app.get("/weed/:id", jwtMW, (req, res) =>
     db.weed.findById(req.params.id).then(result => res.json(result))
