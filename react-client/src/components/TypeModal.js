635ad99fef43f169c7717fc12aa66938acb2d80a
Anton Sawka <antonsawka2@gmail.com>
2019-10-30 16:11:37 -0500

    420

--- react-client/src/components/TypeModal.js
+++ react-client/src/components/TypeModal.js
@@ -23,7 +23,7 @@ const TypeModal = props => {
           width="40%"
           flexDirection="column"
         >
-          <h3>THC</h3>
+          <h3>thc</h3>
           THC is one of the main components in cannabis and can alter brain function. It's effects are often described as euphoric and relaxing. The higher the percentage, the stronger the effects. Products with high THC and low CBD can produce stronger psychoactive effects.
         </Flex>
         <Flex 
@@ -31,7 +31,7 @@ const TypeModal = props => {
           width="40%"
           flexDirection="column"
         >
-          <h3>CBD</h3>
+          <h3>cbd</h3>
           CBD is another component of cannabis. It can create more of a body high, reducing stress and contributing to pain relief. Products with high CBD and low THC can produce more intense body highs. When a product has similar THC and CBD, the CBD levels can reduce the psychoactive effects of the THC.
         </Flex>
       </Flex>
