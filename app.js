90867d16cfa1bb8aec5e04fc4e2fe747109e6d38
Eric Kulchycki <erickulchycki@gmail.com>
2019-11-19 21:36:55 -0600

    Change paths

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "react-client/build")));
 
 const jwtMW = exjwt({
   secret: "XxSmonkWeedErrday420xX"
